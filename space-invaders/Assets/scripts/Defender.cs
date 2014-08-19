f3d79371331c640371a3ef6beccaa23f5767d567
Gonzalo Castiglione <gcastigl@gmail.com>
2014-08-19 19:51:06 -0300

    version 0.1 del space invaders.

---
+++ space-invaders/Assets/scripts/Defender.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Defender : MonoBehaviour {
+
+	public Vector2 speed;
+	private Vector2 movement;
+
+	void Update() {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		movement = new Vector2(speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate() {
+		// Debug.Log (movement);
+		rigidbody2D.velocity = movement;
+	}
+}
---
+++ space-invaders/Assets/scripts/InvaderLine.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvaderLine : MonoBehaviour {
+
+	public float xSpeed;
+	public Vector2 xLimits;
+
+	private Vector2 velocity;
+
+	void Start () {
+		velocity = new Vector2(xSpeed, 0);
+	}
+
+	void Update() {
+		if (escapedMinX() || escapedMaxX()) {
+			velocity = velocity * -1;
+		}
+	}
+
+	private bool escapedMaxX() {
+		return transform.position.x < xLimits.x && velocity.x < 0; 
+	}
+
+	private bool escapedMinX() {
+		return transform.position.x > xLimits.y && velocity.x > 0;
+	}
+
+	void FixedUpdate () {
+		rigidbody2D.velocity = velocity;
+	}
+}
