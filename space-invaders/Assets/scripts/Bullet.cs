da8c3bc2d69a41b5d9a44cbb78ccea0e9372ada4
Gonzalo Castiglione <gcastigl@gmail.com>
2014-08-20 19:15:10 -0300

    Ahora el defender puede disparar utilziando la barra espaciadora (se le asocio un Gun)

---
+++ space-invaders/Assets/scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+	
+	public Vector2 velocity;
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		rigidbody2D.velocity = velocity;
+	}
+}
---
+++ space-invaders/Assets/scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public float delay;
+	public float bulletSpeed;
+	private float delayUnitNextShoot = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update () {
+		if (delayUnitNextShoot > 0) {
+			delayUnitNextShoot -= Time.deltaTime;
+			delayUnitNextShoot = Mathf.Max(0f, delayUnitNextShoot);
+		}
+		if (delayUnitNextShoot == 0 && Input.GetKeyDown(KeyCode.Space)) {
+			delayUnitNextShoot = delay;
+			Vector3 position = transform.position;
+			GameObject bulletGO = Object.Instantiate(bulletPrefab, position, Quaternion.identity) as GameObject;
+			Bullet bullet = bulletGO.GetComponent<Bullet>();
+			bullet.velocity = new Vector2(0, 1) * bulletSpeed;
+		}
+	}
+}
