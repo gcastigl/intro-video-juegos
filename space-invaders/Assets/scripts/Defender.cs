1e0818ef0c593d99e274e5fb6882cc3d82b3436c
Gonzalo Castiglione <gcastigl@gmail.com>
2014-08-26 18:23:35 -0300

    el defender ya no puede pasarse de los limites de la pantalla. los escudos tenian un valor incorrecto para la cantidad de tiros.

--- space-invaders/Assets/scripts/Defender.cs
+++ space-invaders/Assets/scripts/Defender.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Defender : MonoBehaviour {
 	
 	public Vector2 speed;
+	public Vector2 xBounds;
 	private Vector2 movement;
 	private Gun gun;
 
@@ -13,11 +14,23 @@ public class Defender : MonoBehaviour {
 
 	void Update() {
 		float inputX = Input.GetAxis("Horizontal");
-		//float inputY = Input.GetAxis("Vertical");
+		if (outOfBoundsLeft()) {
+			inputX = Mathf.Max(0, inputX);
+		} else if (outOfBoundsRight()) {
+			inputX = Mathf.Min(0, inputX);
+		}
 		movement = new Vector2(speed.x * inputX, 0);
 		if (Input.GetKey(KeyCode.Space)) {
 			gun.shoot(new Vector3(0, 1, 0));
 		}
+	}
+
+	public bool outOfBoundsLeft() {
+		return transform.position.x < xBounds.x;
+	}
+
+	public bool outOfBoundsRight() {
+		return transform.position.x > xBounds.y;
 	}
 
 	void FixedUpdate() {
