9241b7d27e2bbeca67f4598e11530557323ceabc
Gonzalo Castiglione <gcastigl@gmail.com>
2014-09-23 23:37:45 -0300

    se agrega target. ahora el juego se puede perde y ganar.

--- offroad/Assets/Scripts/OffroadGame.cs
+++ offroad/Assets/Scripts/OffroadGame.cs
@@ -3,33 +3,54 @@ using System.Collections;
 
 public class OffroadGame : MonoBehaviour {
 
+	public float remaningSeconds = 60;
+
 	private bool lost;
 	private bool beaten;
-	public float remaningSeconds = 60 * 1000;
 
 	void Start () {
 	}
 
 	void Update () {
 		remaningSeconds -= Time.deltaTime;
+		if (remaningSeconds <= 0) {
+			setLost();
+		}
 	}
 
 	void OnGUI() {
-		int seconds = (int) remaningSeconds;
-		GUI.Label(new Rect(Screen.width - 120, 10, 100, 20), "Tiempo: " + seconds + " [Seg]");
 		if (beaten) {
-			GUI.Label(new Rect(10, 10, 100, 20), "Ganaste!!");
+			GUI.Label (new Rect (Screen.width / 2 - 50, Screen.height / 2, 100, 20), "A winner is you! =)");
 		} else if (lost) {
-			GUI.Label(new Rect(10, 10, 100, 20), "Perdiste =(");
+			GUI.Label (new Rect (Screen.width / 2 - 110, Screen.height / 2, 220, 20), "Perdiste! Too hard for you?");
+		} else {
+			int seconds = (int) remaningSeconds;
+			GUI.Label(new Rect(Screen.width - 120, 10, 100, 20), "Tiempo: " + seconds + " [Seg]");
+		}
+		if (isEndOfgame()) {
+			GUI.Label (new Rect (Screen.width / 2 - 125, Screen.height / 2 + 20, 250, 20), "Enter para volver al menu principal");
+			GUI.Label (new Rect (Screen.width / 2 - 50, Screen.height / 2 + 40, 100, 20), "R para reiniciar");
+			if (Input.GetKey(KeyCode.Return)) {
+				Application.LoadLevel("main-menu");
+			}
+			if (Input.GetKey("r")) {
+				Application.LoadLevel (Application.loadedLevelName);
+			}
+
 		}
 	}
 
 	public void setBeaten() {
 		beaten = true;
+		Time.timeScale = 0;
 	}
 
 	public void setLost() {
-		beaten = true;
+		lost = true;
+		Time.timeScale = 0;
 	}
 
+	public bool isEndOfgame() {
+		return beaten || lost;
+	}
 }
