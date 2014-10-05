14bee5cd5c0226c79909aeda3fc8fc0580c89854
Gonzalo Castiglione <gcastigl@gmail.com>
2014-10-05 20:24:50 -0300

    se agregan treasure chest. se agregan spawners. todos los chests y spawners se crean en runtime a partir de la informacion del properties del mapa. cambio el mapa, pongo uno nuevo.

---
+++ dungeon-crawler/Assets/Scripts/ChestOpen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestOpen : MonoBehaviour {
+
+	public Animation chestAnimation;
+	
+	void Start () {
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == 9) {
+			Debug.Log ("Treasure chest opened!!");
+			chestAnimation.Play();
+			Destroy(this);
+		}
+	}
+}
---
+++ dungeon-crawler/Assets/Scripts/DungeonLoader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class DungeonLoader : MonoBehaviour {
+
+	public GameObject[] items;
+	public GameObject itemNode;
+	public TextAsset itemsAsset;
+
+	public GameObject[] spawns;
+	public GameObject spawnsNode;
+	public TextAsset spawnsAsset;
+
+	void Start () {
+		load(itemsAsset, items, itemNode);
+		load(spawnsAsset, spawns, spawnsNode);
+		Destroy (this);
+	}
+
+	private void load(TextAsset asset, GameObject[] objects, GameObject node) {
+		string[] words = asset.text.Split('\n');
+		foreach (string line in words) {
+			string[] values = line.Split(' ');
+			int x = int.Parse(values[1]);
+			int z = int.Parse(values[2]);
+			int type = int.Parse(values[3]);
+			GameObject item = Object.Instantiate(objects[type]) as GameObject;
+			item.transform.parent = node.transform;
+			item.transform.position = new Vector3(x, 1, z);
+		}
+	}
+
+	void Update () {
+	
+	}
+}
