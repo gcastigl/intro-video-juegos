2f3a7d2c2186e2d3492c8580375b84307e58498d
Gonzalo Castiglione <gcastigl@gmail.com>
2014-10-11 18:09:05 -0300

    Cambio el material de la caverna por este que subo.

--- dungeon-crawler/Assets/Scripts/Player.cs
+++ dungeon-crawler/Assets/Scripts/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour {
 	void Start () {
 		torchIsHight = true;
 		torcheLight = GetComponentInChildren<Torchelight>();
+		torcheLight.IntensityLight = highTorch.x;
+		torcheLight.MaxLightIntensity = highTorch.y;
 	}
 	
 	void Update () {
