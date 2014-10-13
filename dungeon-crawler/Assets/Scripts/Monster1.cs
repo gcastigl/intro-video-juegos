08bc4e9b4b4eaaac27d70841b5c6a97e4a2463e5
Gonzalo Castiglione <gcastigl@gmail.com>
2014-10-13 18:59:37 -0300

    los cofres ya no se ven en la oscuridad. los monstruos persiguen al jugador si este tiene la antorcha prendida, lo ignoran sino. los monstros ahora pueden "ver".

--- dungeon-crawler/Assets/Scripts/Monster1.cs
+++ dungeon-crawler/Assets/Scripts/Monster1.cs
@@ -3,32 +3,58 @@ using System.Collections;
 
 public class Monster1 : MonoBehaviour {
 
+	public int playerLayerMask;
 	public float moveSpeed = 1;
 	public float turnSpeed = 1;
-	public float viewDistance = 30;
+	public float viewDistance = 15;
 
 	private Player player;
+
+	// XXX: CharacterMotor is defined in JS, ignore compile error...
+	private CharacterMotor motor;
 
 	void Start () {
 		GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
 		player = playerGO.GetComponent<Player>();
 	}
-	
+
+	void Awake () {
+		motor = GetComponent<CharacterMotor> ();
+	}
+
 	void Update () {
+		motor.inputMoveDirection = Vector3.zero;
 		// FIXME: no existe un distanceSq?? (Chequeo mas eficiente)
 		float distance = Vector3.Distance(player.transform.position, transform.position);
-		float computedViewDistance = distance * (player.isTorchHigh() ? 1 : 0.5f);
-		if (computedViewDistance > viewDistance) {
+		if (!player.isTorchHigh()) {	// Cant see in the dark!
+			Vector3 forward = transform.TransformDirection(Vector3.forward);
+			motor.inputMoveDirection = forward.normalized * 0.05f;
+			if (Random.value < 0.3f) {
+				float angle = Random.value * 15 - 7.5f;
+				transform.Rotate(new Vector3(0, angle, 0));
+			}
 			return;
 		}
+		if (distance > viewDistance) {		// Player is too far away
+			return;
+		}
+		Vector3 direction = player.transform.position - transform.position;
+		Ray ray = new Ray (transform.position, direction.normalized);
+		RaycastHit hitInfo = new RaycastHit ();
+		bool hit = Physics.Raycast(ray, out hitInfo, viewDistance);
+		if (hit && hitInfo.transform.gameObject.layer != playerLayerMask) {
+			// PLayer is behind something...
+			return;
+		}
+		Quaternion lookAt = Quaternion.LookRotation (direction);
+		float str = Mathf.Min (turnSpeed * Time.deltaTime, 1); 
+		transform.rotation = Quaternion.Lerp(transform.rotation, lookAt, str);
 		if (distance < 2) {
 			return;
 		}
-		Debug.Log ("Detectado: " + name);
-		Vector3 direction = player.transform.position - transform.position;
-		Vector3 moveVector = direction.normalized * moveSpeed * Time.deltaTime;
-		transform.position += moveVector;
-		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
+		if (Mathf.Abs(str) < 0.04f) {
+			motor.inputMoveDirection = direction.normalized;
+		}
 	}
 
 }
