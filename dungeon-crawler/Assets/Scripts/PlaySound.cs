74e3f47ecdb6d29e596ec567cd977e11363da205
Federelli <federelli@gmail.com>
2014-10-19 23:13:47 -0300

    Primera Versión jugable completa del juego

---
+++ dungeon-crawler/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider otherObj){
+		if (!audio.isPlaying) {
+			audio.Play ();
+		}
+	}
+
+}
---
+++ dungeon-crawler/Assets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportPlayer : MonoBehaviour {
+
+	public GameObject roomStartingPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider otherObj)
+	{
+		otherObj.transform.position = roomStartingPosition.transform.position;
+	}
+}
