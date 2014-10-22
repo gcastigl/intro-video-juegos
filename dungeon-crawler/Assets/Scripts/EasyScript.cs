3e8733213bd3970a77da0dd84a1564853d7de0d5
Federelli <federelli@gmail.com>
2014-10-22 18:29:51 -0300

    Se agregan los settings de dificultad

---
+++ dungeon-crawler/Assets/Scripts/EasyScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasyScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider otherObj)
+	{
+		PlayerPrefs.SetInt ("Difficulty", 1);
+	}
+}
