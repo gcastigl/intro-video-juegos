6fe609a798d844bb538af00dd0aae94ffecd7b8c
Gonzalo Castiglione <gcastigl@gmail.com>
2014-10-23 07:07:00 -0200

    press h to see game controls

---
+++ dungeon-crawler/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	public GameObject helpMenu;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.H)) {
+			helpMenu.SetActive(!helpMenu.activeInHierarchy);
+		}
+	}
+}
