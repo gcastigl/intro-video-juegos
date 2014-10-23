01b869691147d9a37091335abeeda44932d18b48
Federelli <federelli@gmail.com>
2014-10-22 21:22:14 -0300

    Mejoras infinitas al home scene

---
+++ dungeon-crawler/Assets/Scripts/StrobeLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StrobeLight : MonoBehaviour {
+
+	public float time = .5f; //time between on and off
+	
+	void Start () {
+		StartCoroutine("Flicker");
+	}
+
+	IEnumerator Flicker(){
+		while(true){
+			light.enabled = false;
+			yield return new WaitForSeconds(time);
+			light.enabled = true;
+			yield return new WaitForSeconds(time);
+		}
+	}
+}
