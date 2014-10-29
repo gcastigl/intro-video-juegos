e6db1234f6641fb7de1082ee70eacae72cba97e5
Federelli <federelli@gmail.com>
2014-10-28 21:35:59 -0300

    Se agrega un zombie para cagarte hasta las patas

---
+++ dungeon-crawler/Assets/MonsterDisappear.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterDisappear : MonoBehaviour {
+
+	void OnTriggerEnter(Collider otherObj){
+		this.gameObject.SetActive (false);
+	}
+}
